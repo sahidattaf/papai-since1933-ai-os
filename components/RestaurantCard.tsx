b9b1315abf2fc3ec651edf5f57dfb7c6464a8f4d
sahidattaf <148976034+sahidattaf@users.noreply.github.com>
2026-06-30 18:28:22 -0400

    Add restaurant card component

---
+++ components/RestaurantCard.tsx
@@ -0,0 +1,10 @@
+export function RestaurantCard() {
+  return (
+    <section>
+      <h2>Papai Since 1933</h2>
+      <p>Jan Noorduynweg #19-6, Willemstad, Curaçao</p>
+      <p>Open daily from 11am to 12am</p>
+      <p>Reservations: +5999 527 2247</p>
+    </section>
+  );
+}
