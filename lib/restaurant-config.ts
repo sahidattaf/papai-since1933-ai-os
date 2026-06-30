9b046afade6c91e7880617c53a81e60d45be7e3c
sahidattaf <148976034+sahidattaf@users.noreply.github.com>
2026-06-30 18:29:17 -0400

    Add restaurant config helper

---
+++ lib/restaurant-config.ts
@@ -0,0 +1,7 @@
+export const restaurantConfig = {
+  name: "Papai Since 1933",
+  instagram: "@papaisince1933",
+  location: "Jan Noorduynweg #19-6, Willemstad, Curaçao",
+  openingHours: "Daily 11am–12am",
+  reservationPhone: "+5999 527 2247",
+};
