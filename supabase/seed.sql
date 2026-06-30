5634678afd70b4ace8b237430a771da6ded65863
sahidattaf <148976034+sahidattaf@users.noreply.github.com>
2026-06-30 18:22:33 -0400

    Add Supabase seed data

---
+++ supabase/seed.sql
@@ -0,0 +1,2 @@
+insert into restaurants (name, instagram, location, opening_hours, reservation_phone)
+values ('Papai Since 1933', '@papaisince1933', 'Jan Noorduynweg #19-6, Willemstad, Curaçao', 'Daily 11am–12am', '+5999 527 2247');
